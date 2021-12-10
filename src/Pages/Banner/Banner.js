b3ecb8daed6a3602f6d8cc1233bcfa6ddf431448
hasibdurjoy <hrd2571@gmail.com>
2021-12-10 22:40:53 +0600

    everything is ok

--- src/Pages/Banner/Banner.js
+++ src/Pages/Banner/Banner.js
@@ -2,22 +2,28 @@ import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import bannerImage from '../../Utilities/Images/banner.png';
 import './Banner.css';
+import Bounce from 'react-reveal/Bounce';
+import Zoom from 'react-reveal/Zoom';
 
 const Banner = () => {
     return (
         <Container className="mt-4">
             <Row>
                 <Col sm={12} md={6} lg={6} className='d-flex align-items-center'>
-                    <div className='d-flex align-items-center'>
-                        <div>
-                            <h1 className="mb-4 banner-title">Want anything to  be <br /> easy with <span style={{ fontWeight: 700 }}>LaslesVPN</span></h1>
-                            <p className="mb-4 text-secondary">Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</p>
-                            <Button className="mt-2 rounded px-5 py-2 border-0 banner-button">Get Started</Button>
+                    <Bounce left big>
+                        <div className='d-flex align-items-center'>
+                            <div>
+                                <h1 className="mb-4 banner-title">Want anything to  be <br /> easy with <span style={{ fontWeight: 700 }}>LaslesVPN</span></h1>
+                                <p className="mb-4 text-secondary">Provide a network for all your needs with ease and fun using LaslesVPN discover interesting features from us.</p>
+                                <Button className="mt-2 rounded px-5 py-2 border-0 banner-button">Get Started</Button>
+                            </div>
                         </div>
-                    </div>
+                    </Bounce>
                 </Col>
                 <Col sm={12} md={6} lg={6}>
-                    <img src={bannerImage} alt="" className="img-fluid banner-image" />
+                    <Zoom>
+                        <img src={bannerImage} alt="" className="img-fluid banner-image" />
+                    </Zoom>
                 </Col>
             </Row>
         </Container >
--- src/Pages/Plan/Plan.js
+++ src/Pages/Plan/Plan.js
@@ -1,103 +1,116 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import cardImage from '../../Utilities/Images/card-image.png'
+import cardImage from '../../Utilities/Images/card-image.png';
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
 
 const Plan = () => {
     return (
         <Container className="mt-5">
-            <h2 className='text-center' style={{ fontWeight: 500, fontSize: "35px" }}>Choose Your Plan</h2>
-            <p className='text-center text-secondary'>Let's choose the package that is best for you and explore it happily and <br /> cheerfully.</p>
-
-            <Row xs={1} sm={2} md={3} className="g-4 mt-3">
-                <Col>
-                    <Card className="p-4 h-100">
-                        <Card.Img variant="top" src={cardImage} className="img-fluid w-50 mx-auto" />
-                        <Card.Body>
-                            <Card.Title className='text-center mb-3'>Free Plan</Card.Title>
-                            <Card.Text className="text-start ms-5">
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Unlimited Bandwitch</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Encrypted Connection</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}No Traffic Logs</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Works on All Devices</p>
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer style={{ backgroundColor: "white" }} className='text-center border-0'>
-                            <h2 >Free</h2>
-                            <button className="btn  w-75 rounded-pill mx-auto" style={{ border: "2px solid #F53838", fontWeight: 500 }}>Select</button>
-                        </Card.Footer>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="p-4 h-100">
-                        <Card.Img variant="top" src={cardImage} className="img-fluid w-50 mx-auto" />
-                        <Card.Body>
-                            <Card.Title className='text-center mb-3'>Standard Plan</Card.Title>
-                            <Card.Text className="text-start ms-5">
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Unlimited Bandwitch</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Encrypted Connection</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Yes Traffic Logs</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Works on All Devices</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Connect Anyware</p>
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer style={{ backgroundColor: "white" }} className='text-center border-0'>
-                            <h2 >$9/mo</h2>
-                            <button className="btn  w-75 rounded-pill mx-auto" style={{ border: "2px solid #F53838", fontWeight: 500 }}>Select</button>
-                        </Card.Footer>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="p-4 h-100 rounded" style={{ border: "2px solid #F53838" }}>
-                        <Card.Img variant="top" src={cardImage} className="img-fluid w-50 mx-auto" />
-                        <Card.Body>
-                            <Card.Title className='text-center mb-3'>Premium Plan</Card.Title>
-                            <Card.Text className="text-start ms-5">
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Unlimited Bandwitch</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Encrypted Connection</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Yes Traffic Logs</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Works on All Devices</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Connect Anyware</p>
-                                <p className="text-secondary">
-                                    <i className="fas fa-check text-success me-3"></i>
-                                    {' '}Get New Features</p>
-                            </Card.Text>
-                        </Card.Body>
-                        <Card.Footer style={{ backgroundColor: "white" }} className='text-center border-0'>
-                            <h2 >$12/mo</h2>
-                            <button className="btn  w-75 rounded-pill mx-auto" style={{ backgroundColor: "#F53838", fontWeight: 500, color: "white" }}>Select</button>
-                        </Card.Footer>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
+            <Fade left cascade>
+                <h2 className='text-center' style={{ fontWeight: 500, fontSize: "35px" }}>Choose Your Plan</h2>
+            </Fade>
+            <Fade right cascade>
+                <p className='text-center text-secondary'>Let's choose the package that is best for you and explore it happily and <br /> cheerfully.</p>
+            </Fade>
+            <Zoom bottom >
+                <Row xs={1} sm={2} md={3} className="g-4 mt-3">
+                    <Col>
+                        <Card className="p-4 h-100">
+                            <Card.Img variant="top" src={cardImage} className="img-fluid w-50 mx-auto" />
+                            <Card.Body>
+                                <Card.Title className='text-center mb-3'>Free Plan</Card.Title>
+                                <Card.Text className="text-start ms-5">
+                                    <Zoom bottom cascade>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Unlimited Bandwitch</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Encrypted Connection</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}No Traffic Logs</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Works on All Devices</p>
+                                    </Zoom>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer style={{ backgroundColor: "white" }} className='text-center border-0'>
+                                <h2 >Free</h2>
+                                <button className="btn  w-75 rounded-pill mx-auto" style={{ border: "2px solid #F53838", fontWeight: 500 }}>Select</button>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="p-4 h-100">
+                            <Card.Img variant="top" src={cardImage} className="img-fluid w-50 mx-auto" />
+                            <Card.Body>
+                                <Card.Title className='text-center mb-3'>Standard Plan</Card.Title>
+                                <Card.Text className="text-start ms-5">
+                                    <Zoom bottom cascade>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Unlimited Bandwitch</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Encrypted Connection</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Yes Traffic Logs</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Works on All Devices</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Connect Anyware</p>
+                                    </Zoom>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer style={{ backgroundColor: "white" }} className='text-center border-0'>
+                                <h2 >$9/mo</h2>
+                                <button className="btn  w-75 rounded-pill mx-auto" style={{ border: "2px solid #F53838", fontWeight: 500 }}>Select</button>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card className="p-4 h-100 rounded" style={{ border: "2px solid #F53838" }}>
+                            <Card.Img variant="top" src={cardImage} className="img-fluid w-50 mx-auto" />
+                            <Card.Body>
+                                <Card.Title className='text-center mb-3'>Premium Plan</Card.Title>
+                                <Card.Text className="text-start ms-5">
+                                    <Zoom bottom cascade>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Unlimited Bandwitch</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Encrypted Connection</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Yes Traffic Logs</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Works on All Devices</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Connect Anyware</p>
+                                        <p className="text-secondary">
+                                            <i className="fas fa-check text-success me-3"></i>
+                                            {' '}Get New Features</p>
+                                    </Zoom>
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer style={{ backgroundColor: "white" }} className='text-center border-0'>
+                                <h2 >$12/mo</h2>
+                                <button className="btn  w-75 rounded-pill mx-auto" style={{ backgroundColor: "#F53838", fontWeight: 500, color: "white" }}>Select</button>
+                            </Card.Footer>
+                        </Card>
+                    </Col>
+                </Row>
+            </Zoom>
+        </Container >
     );
 };
 

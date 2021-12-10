d67be59ee8281d9883a837112654c43790f487eb
hasibdurjoy <hrd2571@gmail.com>
2021-12-11 00:40:46 +0600

    animation updated

--- src/Pages/Testimonial/Testimonial.js
+++ src/Pages/Testimonial/Testimonial.js
@@ -132,21 +132,21 @@ const Testimonial = () => {
                             </Card.Body>
                         </Card>
                     </div>
-                    <div class="item mx-3" style={{ height: "230px" }}>
-                        <Card className='px-3 py-4 h-100 rounded  mb-5'>
+                    <div class="item mx-3 mb-5" style={{ height: "230px" }}>
+                        <Card className='px-3 py-4 h-100 rounded border-danger mb-5'>
                             <Row>
-                                <Col sm={8} md={8} lg={8}>
+                                <Col sm={9} md={9} lg={9}>
                                     <Row className='text-start'>
                                         <Col sm={4} md={4} lg={4}>
                                             <img src={icon1} alt="" className='card-icon' />
                                         </Col>
                                         <Col sm={8} md={8} lg={8} className='text-start'>
                                             <h6>Viezh Robert</h6>
-                                            <p className='text-secondary'>Designation</p>
+                                            <p className='text-secondary' style={{ fontSize: "14px" }}>Warsaw, Poland</p>
                                         </Col>
                                     </Row>
                                 </Col>
-                                <Col sm={4} md={4} lg={4}>
+                                <Col sm={3} md={3} lg={3}>
                                     <p className='d-flex align-items-center'>4.5 <i className="ms-2 fas fa-star card-star"></i></p>
                                 </Col>
                             </Row>
@@ -155,6 +155,62 @@ const Testimonial = () => {
                                 <Card.Text>
                                     <Fade left cascade>
                                         “Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best”.
+                                    </Fade>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div class="item mx-3" style={{ height: "230px" }}>
+                        <Card className='px-3 py-4 h-100 rounded  mb-5'>
+                            <Row>
+                                <Col sm={9} md={9} lg={9}>
+                                    <Row className='text-start'>
+                                        <Col sm={4} md={4} lg={4}>
+                                            <img src={icon2} alt="" className='card-icon' />
+                                        </Col>
+                                        <Col sm={8} md={8} lg={8} className='text-start'>
+                                            <h6>Yessica Christy</h6>
+                                            <p className='text-secondary' style={{ fontSize: "14px" }}>Shanxi, China</p>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col sm={3} md={3} lg={3}>
+                                    <p className='d-flex align-items-center'>4.5 <i className="ms-2 fas fa-star card-star"></i></p>
+                                </Col>
+                            </Row>
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <Fade left cascade>
+                                        “I like it because I like to travel far and still can connect with high speed.”.
+                                    </Fade>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div class="item mx-3" style={{ height: "230px" }}>
+                        <Card className='px-3 py-4 h-100 rounded  mb-5'>
+                            <Row>
+                                <Col sm={9} md={9} lg={9}>
+                                    <Row className='text-start'>
+                                        <Col sm={4} md={4} lg={4}>
+                                            <img src={icon3} alt="" className='card-icon' />
+                                        </Col>
+                                        <Col sm={8} md={8} lg={8} className='text-start'>
+                                            <h6>Kim Young Jou</h6>
+                                            <p className='text-secondary' style={{ fontSize: "14px" }}>Seoul, South Korea</p>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                                <Col sm={3} md={3} lg={3}>
+                                    <p className='d-flex align-items-center'>4.5 <i className="ms-2 fas fa-star card-star"></i></p>
+                                </Col>
+                            </Row>
+
+                            <Card.Body>
+                                <Card.Text>
+                                    <Fade left cascade>
+                                        “This is very unusual for my business that currently requires a virtual private network that has high security.”.
                                     </Fade>
                                 </Card.Text>
                             </Card.Body>
